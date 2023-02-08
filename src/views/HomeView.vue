659f2f3f313428ca9d4e93e92157c138bbb09c49
Alaa <alaa.ahmedgamal10.com>
2023-02-08 17:48:38 +0200

    setup project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <div class="text-red-500">this is color div tailwind</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
