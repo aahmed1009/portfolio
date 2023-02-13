13e1157323b26c7474f9acf2d4be257a98d7d534
Andrew <AndrewMelad>
2023-02-13 18:15:47 +0200

    add section header and about

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="home">
-    <div class="text-red-500">this is color div tailwind</div>
+  <div class="dark" id="container">
+    <div
+      class="bg-gradient-to-l from-cyan-50 dark:from-gray-900 dark:via-gray-700 dark:to-gray-900 lg:px-20 md:px-10 ms:px-5 min-h-screen"
+    >
+      <button id="mode" v-on:click="showMode()">{{ contant }}</button>
+      <headerSection />
+      <aboutSection />
+      <projectSection />
+    </div>
   </div>
 </template>
 
 <script>
+import headerSection from "../components/header.vue";
+import aboutSection from "../components/AboutUs.vue";
+import projectSection from "../components/PorjectSection.vue";
 export default {
   name: "HomeView",
+  data() {
+    return {
+      contant: "dark",
+    };
+  },
+  components: {
+    headerSection,
+    aboutSection,
+    projectSection,
+  },
+  methods: {
+    showMode() {
+      let body = document.getElementById("container");
+      if (this.contant == "dark") {
+        this.contant = "dark";
+      } else {
+        this.contant = "light";
+      }
+      body.classList.toggle("dark");
+    },
+  },
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 2 column grid
+        "2-auto": "repeat(2, minmax(0, auto))",
+      },
+      colors: {
+        asd: "#ddd",
+      },
+      container: {
+        center: true,
+      },
+    },
   },
   variants: {
     extend: {},
