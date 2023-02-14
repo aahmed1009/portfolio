a58c0d6e527eb11c3d560264ecb88ccbe877974b
Alaa <alaa.ahmedgamal10.com>
2023-02-14 11:28:14 +0200

    project section

--- src/components/PorjectSection.vue
+++ src/components/PorjectSection.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="projects h-96 bg-gradient-to-l from-white to-teal-200 rounded-2xl px-6 py-2 dark:from-cyan-700 dark:to-cyan-900 dark:text-white grid"
-  >
+  <div class="projects h-96 bg-gradient-to-l from-white to-teal-200 rounded-2xl px-6 py-2 dark:from-cyan-700 dark:to-cyan-900 dark:text-white grid">
     <h1 class="text-center pt-10 font-bold text-3xl capitalize">
       Click the project for me details
     </h1>
