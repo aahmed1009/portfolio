e4038d2d3f32ae43263c9a3b3293e353345ed58c
Andrew <AndrewMelad>
2023-02-16 16:36:00 +0200

    slider

--- src/components/PorjectSection.vue
+++ src/components/PorjectSection.vue
@@ -8,95 +8,89 @@
     >
       <div
         class="p-3 bg-teal-300 h-auto rounded-xl w-fit cursor-pointer"
-        data-modal-target="asd"
-        data-modal-toggle="ASD"
+        v-for="project in dataInfo"
+        :key="project.id"
+        :data-modal-target="project.id"
+        :data-modal-toggle="project.id"
+        type="section1"
       >
-        <img src="../assets/Screenshot from 2023-02-15 11-46-52.png" alt="" />
-        <p class="mt-2 text-2xl font-bold dark:text-white">project</p>
-      </div>
-
-      <div
-        class="p-3 bg-teal-300 h-auto rounded-xl w-fit cursor-pointer"
-        data-modal-target="defaultModal"
-        data-modal-toggle="defaultModal"
-      >
-        <img src="../assets/Screenshot from 2023-02-15 11-46-52.png" alt="" />
-        <p class="mt-2 text-2xl font-bold dark:text-white">project</p>
-      </div>
-      <div
-        class="p-3 bg-teal-300 h-auto rounded-xl w-fit cursor-pointer"
-        data-modal-target="defaultModal"
-        data-modal-toggle="defaultModal"
-        id="1"
-      >
-        <img src="../assets/Screenshot from 2023-02-15 11-46-52.png" alt="" />
-        <p class="mt-2 text-2xl font-bold dark:text-white">project</p>
-      </div>
-      <div
-        class="p-3 bg-teal-300 h-auto rounded-xl w-fit cursor-pointer"
-        data-modal-target="defaultModal"
-        data-modal-toggle="defaultModal"
-      >
-        <img src="../assets/Screenshot from 2023-02-15 11-46-52.png" alt="" />
-        <p class="mt-2 text-2xl font-bold dark:text-white">project</p>
-      </div>
-      <div
-        class="p-3 bg-teal-300 h-auto rounded-xl w-fit cursor-pointer"
-        data-modal-target="defaultModal"
-        data-modal-toggle="defaultModal"
-      >
-        <img src="../assets/Screenshot from 2023-02-15 11-46-52.png" alt="" />
-        <p class="mt-2 text-2xl font-bold dark:text-white">project</p>
-      </div>
-      <!-- Main modal -->
-      <div
-        id="defaultModal"
-        tabindex="-1"
-        aria-hidden="true"
-        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
-      >
-        <div class="relative w-full h-full max-w-2xl md:h-auto">
-          <!-- Modal content -->
-          <div
-            class="fixed bottom-0 left-0 right-0 bg-white rounded-lg shadow dark:bg-cyan-900 h-[400px] w-[100vw]"
-          >
-            <!-- Modal header -->
+        <img :src="require(`../assets/${project.image}`)" alt="asd" />
+        <p class="mt-2 text-2xl font-bold dark:text-white">
+          {{ project.name }}
+        </p>
+        <!-- Main modal -->
+        <div
+          :id="project.id"
+          tabindex="-1"
+          aria-hidden="true"
+          class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
+        >
+          <div class="relative w-full h-full max-w-2xl md:h-auto">
+            <!-- Modal content -->
             <div
-              class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600"
+              class="fixed bottom-0 left-0 right-0 bg-white rounded-lg shadow dark:bg-cyan-900 h-[400px] w-[100vw]"
             >
-              <button
-                type="button"
-                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-hide="defaultModal"
+              <!-- Modal header -->
+              <div
+                class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600"
               >
-                <svg
-                  aria-hidden="true"
-                  class="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                <button
+                  type="button"
+                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="defaultModal"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="sr-only">Close modal</span>
-              </button>
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+                </button>
+              </div>
+              <!-- Modal body -->
+              <div class="grid grid-cols-2-auto">
+                <div class="image-slider p-10 pt-0 w-fit">
+                  <img
+                    src="../assets/Screenshot from 2023-02-15 14-24-52.png"
+                    alt="image"
+                    class="w-56 h-56"
+                  />
+                </div>
+                <div class="des text-start">
+                  <h1 class="text-2xl text-teal-500 my-6">
+                    {{ project.name }}
+                  </h1>
+                  <p class="leading-5">{{ project.description }}</p>
+                  <div class="project-tools mt-10 space-x-7">
+                    <span
+                      class="tool text-center pt-2 inline-block dark:text-black w-20 h-10 rounded-lg dark:bg-gradient-to-t dark:from-teal-800 dark:via-teal-500 dark:to-teal-300"
+                      v-for="tool in project.tools"
+                      :key="tool.index"
+                      >{{ tool }}</span
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="project-links col-start-1 col-end-3 mb-10 h-56">
+                <a
+                  href="#"
+                  class="bg-teal-700 text-start w-28 h-20 rounded-lg block mb-10"
+                  >github</a
+                >
+              </div>
+              <!-- Modal footer -->
+              <div
+                class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
+              ></div>
             </div>
-            <!-- Modal body -->
-            <div
-              class="p-6 space-y-6"
-              v-for="data in dataInfo"
-              :key="data.index"
-            >
-              <span>{{ data.description }}</span>
-            </div>
-            <!-- Modal footer -->
-            <div
-              class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
-            ></div>
           </div>
         </div>
       </div>
